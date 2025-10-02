195d2dbef9ce3b795bdf267997a206cc46f9bb35
Christian King <156232701+CXKing23@users.noreply.github.com>
2025-10-02 10:30:40 -0400

    Add files via upload

---
+++ custom-toolbox-codelab/starter-code/toolbox_label.js
@@ -0,0 +1,20 @@
+class ToolboxLabel extends Blockly.ToolboxItem {
+  constructor(toolboxItemDef, parentToolbox) {
+    super(toolboxItemDef, parentToolbox);
+  }
+  init() {
+    // Create the label.
+    this.label = document.createElement('label');
+    // Set the name.
+    this.label.textContent = 'Label';
+  }
+  /** @override */
+  getDiv() {
+    return this.label;
+  }
+}
+
+Blockly.registry.register(
+  Blockly.registry.Type.TOOLBOX_ITEM,
+  'toolboxlabel',
+  ToolboxLabel);
